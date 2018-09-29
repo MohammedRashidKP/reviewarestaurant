20eb859e678f15a1649d2fa62cb60bc06a165450
mrashid <mrashid@MKARINGA-IN.in.oracle.com>
2018-09-29 22:55:39 +0530

    sdsd

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -46,8 +46,9 @@ def search(request):
 			response = p.search(q="bangalore", establishment_type=searchKeyWord)
 		if searchCategory == 'restaurantCategory':
 			response = p.search(q="bangalore", category=searchKeyWord)
+		print('response = '+response)
 	except:
-		print("Error")
+		print("Error while calling api")
 	try:
 		for num, restaurant in enumerate(response['restaurants']):
 			number= num+1,
@@ -61,10 +62,11 @@ def search(request):
 			res_id=restaurant['restaurant']['id']
 			dictlist = [dict() for x in range(9)]
 			dictlist={'number':number, 'name':name, 'url':url, 'cuisines':cuisines, 'rating':rating, 'icon':icon, 'price':price, 'res_id':res_id }
+			print('dictList='+dictlist)
 			list.append(dictlist)
 	except Exception as e:
 		print(e)
-	return render(request, 'home.html', {'context': list})
+		return render(request, 'home.html', {'context': list})
 	
 @login_required
 def showRestaurantDetails(request, value):
